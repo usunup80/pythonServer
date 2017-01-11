5e407ad2620d02772e9e0e72463f840c261a77d1
이유성 <yousung.lee@navercorp.com>
2017-01-11 17:19:18 +0900

    show html!

--- simpleServer/mysite/urls.py
+++ simpleServer/mysite/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from simpleServer import blog
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls'))
 ]
